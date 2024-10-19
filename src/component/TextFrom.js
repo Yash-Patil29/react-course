2431b488aec1a4f90c48a760222972d67f2d729c
YASH <pyash7267@gmail.com>
2024-10-19 11:57:56 +0530

    Adding more logic to textutils

--- src/component/TextFrom.js
+++ src/component/TextFrom.js
@@ -10,18 +10,34 @@ export default function TextFrom(props) {
         let newtext=text.toUpperCase();
         settext(newtext)
     }
+    const handleLoClick=()=>{
+      // console.log("Upper case was clicked: " + text);
+      let newtext=text.toLowerCase();
+      settext(newtext)
+  }
     const handleonchange=(event)=>{
         // console.log("on change handle");
         settext(event.target.value);
     }
     const [text, settext] = useState('Enter text here');
   return (
+    <>
     <div>
         <h1>{props.heading}</h1>
       <div className="form-group">
-        <textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="8"></textarea>
+        <textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="10"></textarea>
       </div>
-      <button className='btn btn-primary'onClick={handleUpClick}>Convert to Uppercase </button>
+      <button className='btn btn-primary mx-3'onClick={handleUpClick}>Convert to Uppercase </button>
+      <button className='btn btn-primary mx-3'onClick={handleLoClick}>Convert to Lowercase </button>
     </div>
+    <div className="container my-2">
+      <h1>Your text summary</h1>
+      <p> {text.split(" ").length } word {text.length } character</p>
+      <p>{0.008*text.split(" ").length} Minutes to read </p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+
+    </div>
+    </>
   )
 }
