defa816f785562b305310b7e9a82c1d7a9a02556
YASH <pyash7267@gmail.com>
2024-10-19 13:11:11 +0530

    add dark mode button  to textutils

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import Navbar from './component/Navbar';
-import TextFrom from './component/TextFrom';
+  import './App.css';
+  import Navbar from './component/Navbar';
+  import TextFrom from './component/TextFrom';
 
 
-function App() {
-  return (
-    <>
-      <Navbar title ="TextUtils" about="About text"></Navbar>
-      {/*<Navbar></Navbar>*/}
-      <div className="container my-3">
-      <TextFrom heading ="Enter the text to analyze below"></TextFrom>
-      </div>
-      
-    </>
-  );
+  function App() {
+    return (
+      <>
+        <Navbar title ="TextUtils" about="About text"></Navbar>
+        {/*<Navbar></Navbar>*/}
+        <div className="container my-3">
+        <TextFrom heading ="Enter the text to analyze below"></TextFrom>
+        </div>
+        <div class="container-fluid">
+        </div>
+      </>
+    );
 
-}
+  }
 
-export default App;
+  export default App;
--- src/component/TextFrom.js
+++ src/component/TextFrom.js
@@ -1,56 +1,76 @@
-import React,{useState} from 'react'
-import PropTypes from 'prop-types'
+  import React,{useState} from 'react'
+  import PropTypes from 'prop-types'
 
 
 
-// have to add props in bracket to use props or will throw error
-export default function TextFrom(props) {
-    const handleUpClick=()=>{
-        // console.log("Upper case was clicked: " + text);
-        let newtext=text.toUpperCase();
-        settext(newtext)
-    }
-    const handleLoClick=()=>{
-      // console.log("Lower case was clicked: " + text);
-      let newtext=text.toLowerCase();
-      settext(newtext)
-    }
-    const handleClearClick=()=>{
-      // console.log("Clear was clicked: " + text);
-      let newtext='';
-      settext(newtext)
-    }
-    const handleCopyClick=()=>{
-      // console.log("Clear was clicked: " + text);
-      var text=document.getElementById("mybox");
-      text.select();
-      navigator.clipboard.writeText(text.value);
-    }
-    const handleonchange=(event)=>{
-        // console.log("on change handle");
-        settext(event.target.value);
-    }
+  // have to add props in bracket to use props or will throw error
+  export default function TextFrom(props) {
     const [text, settext] = useState('Enter text here');
-  return (
-    <>
-    <div>
-        <h1>{props.heading}</h1>
-      <div className="form-group">
-        <textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="10"></textarea>
-      </div>
-      <button className='btn btn-primary mx-3'onClick={handleUpClick}>Convert to Uppercase </button>
-      <button className='btn btn-primary mx-3'onClick={handleLoClick}>Convert to Lowercase </button>
-      <button className='btn btn-primary mx-3'onClick={handleClearClick}>Clear text </button>
-      <button className='btn btn-primary mx-3'onClick={handleCopyClick}>Copy text </button>
-    </div>
-    <div className="container my-2">
-      <h1>Your text summary</h1>
-      <p> {text.split(" ").length } word {text.length } character</p>
-      <p>{0.008*text.split(" ").length} Minutes to read </p>
-      <h2>Preview</h2>
-      <p>{text}</p>
+    const [isDarkMode, setIsDarkMode] = useState(false); // State to manage dark mode
 
-    </div>
-    </>
-  )
-}
+      const handleUpClick=()=>{
+          // console.log("Upper case was clicked: " + text);
+          let newtext=text.toUpperCase();
+          settext(newtext)
+      }
+      const handleLoClick=()=>{
+        // console.log("Lower case was clicked: " + text);
+        let newtext=text.toLowerCase();
+        settext(newtext)
+      }
+      const handleClearClick=()=>{
+        // console.log("Clear was clicked: " + text);
+        let newtext='';
+        settext(newtext)
+      }
+      const handleCopyClick=()=>{
+        // console.log("Copy was clicked: " + text);
+        var text=document.getElementById("mybox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+      }
+      const handleonchange=(event)=>{
+          // console.log("on change handle");
+          settext(event.target.value);
+      }
+      const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode); // Toggle dark mode
+      };
+      const themeStyles = {
+        backgroundColor: isDarkMode ? '#343a40' : 'white',
+        color: isDarkMode ? 'white' : 'black',
+      };
+      const ButtonStyles = {
+        backgroundColor: isDarkMode ? '#343a40' : 'white',
+        color: isDarkMode ? 'white' : 'black',
+      };
+      
+    return (
+      <>
+      <div className="container" style={themeStyles}>
+          <h1>{props.heading}</h1>
+        <div className="form-group">
+          <textarea className="form-control" value={text} onChange={handleonchange} id="mybox" rows="10"></textarea>
+        </div>
+        <button className='btn btn-primary mx-3'onClick={handleUpClick}style={ButtonStyles}>Convert to Uppercase </button>
+        <button className='btn btn-primary mx-3'onClick={handleLoClick}style={ButtonStyles}>Convert to Lowercase </button>
+        <button className='btn btn-primary mx-3'onClick={handleClearClick}style={ButtonStyles}>Clear text </button>
+        <button className='btn btn-primary mx-3'onClick={handleCopyClick}style={ButtonStyles}>Copy text </button>
+      </div>
+      <div className="container my-2" style={themeStyles}>
+        <h1>Your text summary</h1>
+        <p> {text.split(" ").length } word {text.length } character</p>
+        <p>{0.008*text.split(" ").length} Minutes to read </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+      <button
+          type="button"
+          className='btn btn-dark mx-3'
+          onClick={toggleDarkMode}
+        >
+          {isDarkMode ? 'Disable Dark Mode' : 'Enable Dark Mode'}
+        </button>
+      </>
+    )
+  }
